bd773c0c9538b7834ea108454d3d1b05df0b0f20
Prabhat <prabhatmishra@gmail.com>
2021-10-30 14:55:52 +0530

    Pta nahi kya kiya

--- customtag.js
+++ customtag.js
@@ -1,8 +1,8 @@
 window.onload = () => {
 
     // it's line get t tag
-    let p = document.getElementsByTagName("ph")[0].getAttribute("te");
-    console.log(p);
+    // let p = document.getElementsByTagName("ph")[0].getAttribute("te");
+    // console.log(p);
 
     // if you tag name then you also provie a index no. to get attribute value 
     // if you use id then you use getAttribute() without using index no.
@@ -41,8 +41,38 @@ window.onload = () => {
         t[i].innerHTML = "9:47";
     }
 
+    // welcome tag for full web template for screen
+    we = document.getElementsByTagName("welcome");
+    for (i = 0; i < we.length; i++) {
 
-    
+        var sr = "", bn = "", ql = "", tel = "";
+        if (we[i].getAttribute("sr") != undefined) {
+            sr = we[i].getAttribute("sr");
+        }
+        if (we[i].getAttribute("bn") != undefined) {
+            bn = we[i].getAttribute("bn");
+        }
+        if (we[i].getAttribute("ql") != undefined) {
+            ql = we[i].getAttribute("ql");
+        }
+        if (we[i].getAttribute("tel") != undefined) {
+            tel = we[i].getAttribute("tel");
+        }
+       
+        console.log(sr);
+        var html = "<div id='bi' class='backimg'>";
+        html += "<div class='linear'>";
+        html += "<div class='middle'>";
+        html += "<h1 class='brand-name'>"+ bn +"</h1>";
+        html += "<p class='quato'>"+ ql +"</p>";
+        html += "<h1 class='comming-soon'>Comming Soon</h1>";
+        html += "<a href=' tel:"+ tel +" ' class='clientnumber'>"+ tel +"</a>";
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
+        
+        we[i].innerHTML = html;
+    }
 
 
 }
