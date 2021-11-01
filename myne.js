606cba712d0913b4b303a2a182cb5b4f3d3bbc86
Prabhat <prabhatmishra@gmail.com>
2021-11-01 12:10:16 +0530

    make some list

--- myne.js
+++ myne.js
@@ -3,9 +3,18 @@
 function time() {
     let t = new Date();
     t.toLocaleTimeString();
+    console.log(t);
 }
 
 // setInterval(time, 1000);
 
 
 time();
+
+// it's a validation function list
+
+// number validation function 
+// name validation function
+// Email validation function
+// password validation function 
+// otp validation function 
